3cfa841620b7b2a77510992340ec41557a5ecdc9
Jack-kelly-22 <Jacklaxjk@gmail.com>
2021-01-25 10:46:24 -0800

    second commit

---
+++ dash_app/liveImage.py
@@ -0,0 +1,19 @@
+from dash_app.components import dash_reusable_components as drc
+from utils import image_utils, area_utils
+from skimage.measure import label, regionprops
+from cv2 import resize, INTER_CUBIC
+class LiveImage:
+    def __init__(self,img_data,constants):
+        self.scale_factor = constants["scale"]
+        self.name = 'temp'
+        #img_data = (drc.b64_to_numpy(img_data,False))
+        #img =
+        img = resize(img_data, dsize=(800, 600), interpolation=INTER_CUBIC)
+        img_seg = area_utils.get_thresh_image(img,constants)
+        label_image = label(img_seg,connectivity=1)
+        self.regions = regionprops(label_image)
+        self.porosity = area_utils.get_porosity(img_seg)
+        self.out_image = image_utils.color_out_image(self.regions, img, constants['multi'])
+        self.largest_areas, self.largest_regions,self.largest_circles = area_utils.get_largest_areas(self.regions, self)
+        self.out_image = image_utils.color_out_largest(self.largest_regions, self.out_image)
+        self.out_image = image_utils.color_holes(self.largest_areas,self.out_image)
---
+++ index.py
@@ -0,0 +1,116 @@
+#import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State, MATCH
+from numpy import ndarray
+#import dash_bootstrap_components as dbc
+#import dash_app.components.dash_reusable_components as drc
+import sqlite3
+from utils.sql_utils import convert_array,adapt_array
+import os
+from dash_app.app import app
+#from dash_app.views.navbar import Navbar
+#from dash_app.views.view_job import View_Job
+#from dash_app.views.view_frame import View_Frame
+#from dash_app.views.homepage import Homepage
+#from dash_app.views.new_job import new_job_page
+from dash_app.apps import New_Job_App
+from dash_app.apps import homepage
+from dash_app.apps import View_Job_App
+from dash_app.apps import View_Prev_Jobs
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+import dash
+import threading
+
+
+
+# constants = {
+#     "thresh": None,
+#      "min_ignore": 20.0,
+#      "warn_size": 5000.0,
+#      "scale" : 1.0
+# }
+#
+#
+# #options put in by user these are just placeholders
+# options ={
+#     "program_type": "light",
+#     "input_type":"single",
+#     "job_name": "default_name",
+#     "ref_ls": [],
+#     "name_ls": [],
+#     "constants":constants,
+#     "tags": ["NULL(lol)"]
+#     }
+
+app.layout = html.Div([
+    dcc.Location(id = 'url', refresh= False),
+    html.Div(id = 'page-content')
+])
+server = app.server
+
+@app.callback(Output('page-content','children'), 
+            [Input('url', 'pathname')])
+def display_page(pathname):
+    print("gonna make new layout")
+    if pathname == "/prev-jobs":
+         return View_Prev_Jobs.layout()
+    if pathname == "/new-job":
+
+        return New_Job_App.New_Job()
+    if pathname == "/edit-job":
+        return '404'
+    if '/job_id_' in pathname:
+        print("returning view Job")
+        return View_Job_App.layout
+    else:
+        print("starting homepage")
+        return homepage.layout
+
+
+
+
+"""
+
+@app.callback(
+    Output({'type': 'dynamic-out', 'index': MATCH}, 'children'),
+    Input({'type': 'list-button', 'index': MATCH,}, 'n_clicks'),
+    State({'type': 'list-button', 'index': MATCH}, 'children'),
+)
+def add_out(n_clicks,child):
+    print("this is id",child)
+    print("is this it :",child[0]["props"]["id"])
+    print(type(child))
+    print("so at least it was called tho ")
+    if(n_clicks== 0):
+        return []
+    else:
+        #return [View_Frame(child[0]["props"]["id"])]
+        return []
+
+
+@app.callback(
+    Output(component_id='img-sec', component_property = 'children'),
+    Input({'type': 'list-button'}, 'n_clicks')
+)
+def add_out(n_clicks,):
+    if(n_clicks== 0):
+        return []
+    else:
+        return [html.Div("Test")]
+
+STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+
+
+@app.server.route('/static/<resource>')
+def serve_static(resource):
+    return flask.send_from_directory(STATIC_PATH, resource)
+"""
+
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
+
+
+
